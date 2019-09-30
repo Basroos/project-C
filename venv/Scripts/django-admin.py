7d4a228fb2efb4750c76650454734e8273f451bb
arnorietdijk <arnorietdijk@users.noreply.github.com>
2019-09-30 10:46:08 +0200

    Remove venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\school\hogeschool\leerjaar_2\projectc\project-c\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
