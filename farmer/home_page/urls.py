7954ef6d4f0d6d2a437bbd34395bfe63fc8500a6
EmirSarikus <47593884+EmirSarikus@users.noreply.github.com>
2019-10-02 15:23:16 +0200

    Html and Css structure (not completely done yet)

--- farmer/home_page/urls.py
+++ farmer/home_page/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('', views.home_index, name='home_index'),
-    path('login/', views.login_index, name='login_index')
+    path('login/', views.login_index, name='login_index'),
 ]
