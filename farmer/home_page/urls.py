b908668d55800111704dc9913bdd1e9fd6807ba2
EmirSarikus <47593884+EmirSarikus@users.noreply.github.com>
2019-10-05 17:42:16 +0200

    Register page (but weird error need fix)

--- farmer/home_page/urls.py
+++ farmer/home_page/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from django.conf.urls import url
 from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home_index, name='home_index'),
     path('login/', views.login_index, name='login_index'),
+    path('register/', views.register_index, name='register_index'),
 ]
