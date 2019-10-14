707c05519f97a67f7755700d90faa3b2b851f265
Ahmett70 <47348130+Ahmett70@users.noreply.github.com>
2019-10-14 13:43:49 +0200

    active, username

--- farmer/farmer_page/views.py
+++ farmer/farmer_page/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def farmer_index(request):
-    context = {'info': Farmer.objects.all()}
+    context = {'info': Farmer.objects.all(),
+                'navigation':'farmer'}
     return render(request, 'farmer_page/farmer.html', context)
 
 
