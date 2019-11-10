12df9be95d7a368552028af865f53d43aa8697c6
Ahmett70 <47348130+Ahmett70@users.noreply.github.com>
2019-11-10 11:18:29 +0100

    Add product

--- farmer/user_profile/views.py
+++ farmer/user_profile/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def user_profile(request):
     template_name = 'user_profile/user_profile.html'
     return render(request, template_name)
+
+def add_product(request):
+    template_name = 'user_profile/add_product.html'
+    return render(request, template_name)
