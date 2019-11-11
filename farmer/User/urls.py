b5551ea96ac905485030199346b5745f86441729
EmirSarikus <47593884+EmirSarikus@users.noreply.github.com>
2019-11-11 12:13:03 +0100

    commit

--- farmer/User/urls.py
+++ farmer/User/urls.py
@@ -3,8 +3,14 @@ from . import views
 from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns =[
+<<<<<<< Updated upstream
     path('',views.indexView,name="home_page"),
     path('login/',LoginView.as_view(), name="login_url"),
     path('register/',views.registerView,name="register_url"),
+=======
+    path('',views.indexView,name="home_index"),
+    path('login/',LoginView.as_view(), name="login_url"),
+    path('register/',views.registerView, name="register_url"),
+>>>>>>> Stashed changes
     path('logout/',LogoutView.as_view(next_page='logout'),name="logout"),
 ]
--- farmer/User/views.py
+++ farmer/User/views.py
@@ -3,21 +3,37 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 def indexView(request):
     return render(request, 'home.html')
 
 @login_required
+<<<<<<< Updated upstream
 def LoginView(request):
     return render(request,'home.html')
+=======
+def dashboardView(request):
+    return render(request,'farmer.html')
+>>>>>>> Stashed changes
 
 def registerView(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+<<<<<<< Updated upstream
             return redirect('login_url')  
     else:
             form = UserCreationForm()
 
     return render(request, 'registration/register.html',{'form':form})
+=======
+            return redirect('home_page/home.html')  
+    else:
+            form = UserCreationForm()
+
+    return render(request, 'registration/register.html',{'form':form})
+>>>>>>> Stashed changes
