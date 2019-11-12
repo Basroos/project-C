e82beb07db32821f5778195bfe0228774e6e86d1
Ahmett70 <47348130+Ahmett70@users.noreply.github.com>
2019-11-12 15:20:17 +0100

    Login

--- farmer/User/forms.py
+++ farmer/User/forms.py
@@ -2,17 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-try:
-    class SignUpForm(UserCreationForm):
-        #username = forms.CharField(max_length=40, help_text='Enter a username')
-        email = forms.EmailField(max_length=40, help_text='Enter a valid email adress')
-        name = forms.CharField(max_length=60, required=False, help_text='Enter your full name')
-        farm = forms.CharField(max_length=30, required=False, help_text='Enter the name of your farm/company')
-        phone_number = forms.CharField(max_length=10, help_text='enter a valid phone number')
+class SignUpForm(UserCreationForm):
+    #username = forms.CharField(max_length=40, help_text='Enter a username')
+    email = forms.EmailField(max_length=40, help_text='Enter a valid email adress')
+    name = forms.CharField(max_length=60, required=False, help_text='Enter your full name')
+    farm = forms.CharField(max_length=30, required=False, help_text='Enter the name of your farm/company')
+    phone_number = forms.CharField(max_length=10,help_text='enter a valid phone number')
+    address = forms.CharField(
+        max_length=10, help_text='enter a valid address')
+    age = forms.CharField(
+    max_length=2, help_text='enter your age')
+    province = forms.CharField(
+        max_length=50, help_text='enter your province')
+    products = forms.CharField(
+        max_length=50, help_text='which products do you expect to sell')
 
+    class Meta:
+        model = User
+        fields = ('username', 'name', 'age', 'farm', 'address', 'province','email', 'phone_number', 'products','password1', 'password2')
 
-        class Meta:
-            model = User
-            fields = ('username', 'name', 'farm', 'email', 'phone_number', 'password1', 'password2')
-except:
-    pass
