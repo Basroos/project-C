e650f4363053c41106a4418f32ac551225196fd2
Ahmett70 <47348130+Ahmett70@users.noreply.github.com>
2019-11-12 11:20:30 +0100

    Add product to db

--- farmer/user_profile/models.py
+++ farmer/user_profile/models.py
@@ -8,3 +8,6 @@ class Product(models.Model):
     product_description = models.CharField(max_length=300)
     product_price = models.DecimalField(max_digits=6, decimal_places=2)
     product_user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Farmer " + self.product_user.username + "\tProduct " + self.product_name
