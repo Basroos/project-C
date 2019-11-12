0a438fc9d1c9f0c020716172f9ce9c6ad3d9f650
EmirSarikus <47593884+EmirSarikus@users.noreply.github.com>
2019-11-12 14:03:20 +0100

    Registration is fixed

--- farmer/User/views.py
+++ farmer/User/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView, LogoutView
 from User.forms import SignUpForm
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+
+# Create your views here (Functions or classes.)
 
 def indexView(request):
     return render(request, 'home.html')
 
 def LogoutView(request):
     return render(request,'logout_url')
-    
+
 @login_required
 def LoginView(request):
     return render(request,'home.html')
@@ -23,14 +26,16 @@ def registerView(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            username = form.cleaned_data.get('Username')
+            raw_pass = form.cleaned_data.get('password1')
+            raw_pass = make_password(form.cleaned_data.get('password1'))
+            #user = user.set_password(password1)
+           # login(request, user)
             return redirect('login_url')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
 
     return render(request, 'registration/register.html',{'form':form})
-    
+
+
 
--- farmer/farmer/settings.py
+++ farmer/farmer/settings.py
@@ -104,6 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
