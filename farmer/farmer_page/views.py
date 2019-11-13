24fcb8d10016514794715ef5327d4bf60a79be16
Ahmett70 <47348130+Ahmett70@users.noreply.github.com>
2019-11-13 11:18:41 +0100

    My products profile

--- farmer/farmer_page/views.py
+++ farmer/farmer_page/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Farmer
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 from farmer_page.models import Farmer
 from django.db.models import Q
+from user_profile.models import Product
 # Create your views here.
 
 def farmer_index(request):
@@ -16,6 +17,15 @@ def profile(request, id):
     farmer = get_object_or_404(Farmer, pk=id)
     context = {'farmer':farmer}
     return render(request, 'farmer_page/profile.html', context)
+
+def my_products(request):
+    current_user = request.user.id
+    template = 'farmer_page/my_products.html'
+    profile = get_object_or_404(User, pk=current_user)
+    # products = get_list_or_404(Product, product_user=request.user)
+    products = Product.objects.filter(product_user=request.user)
+
+    return render(request, template, {'profile':profile, 'product': products})
 
 
 def search_farmer(request):
