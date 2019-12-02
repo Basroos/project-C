d64281d6a8eebae6485b027fba94480d93c1a84c
arnorietdijk <arnorietdijk@users.noreply.github.com>
2019-12-02 10:34:20 +0100

    design register pages en login/register button

--- farmer/User/forms.py
+++ farmer/User/forms.py
@@ -8,15 +8,15 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=40, help_text='Enter a valid email adress')
     name = forms.CharField(max_length=60, required=False, help_text='Enter your full name')
     farm = forms.CharField(max_length=30, required=False, help_text='Enter the name of your farm/company')
-    phone_number = forms.CharField(max_length=10,help_text='enter a valid phone number')
+    phone_number = forms.CharField(max_length=10,help_text='Enter a valid phone number')
     address = forms.CharField(
-        max_length=10, help_text='enter a valid address')
+        max_length=10, help_text='Enter a valid address')
     age = forms.CharField(
-    max_length=2, help_text='enter your age')
+    max_length=2, help_text='Enter your age')
     province = forms.CharField(
-        max_length=50, help_text='enter your province')
+        max_length=50, help_text='Enter your province')
     products = forms.CharField(
-        max_length=50, help_text='which products are you expecting to sell')
+        max_length=50, help_text='Which products are you expecting to sell')
 
     class Meta:
         model = User
@@ -26,7 +26,7 @@ class SignUpForm2(UserCreationForm):
     #username = forms.CharField(max_length=40, help_text='Enter a username')
     email = forms.EmailField(max_length=70, help_text='Enter a valid email adress')
     name = forms.CharField(max_length=60, required=False, help_text='Enter your full name')
-    address = forms.CharField(max_length=20, help_text='enter a valid address')
+    address = forms.CharField(max_length=20, help_text='Enter a valid address')
    
     class Meta:
         model = User
