8aa4bd7576679196a002d367fdb72727dfb3ce6b
Ahmett70 <47348130+Ahmett70@users.noreply.github.com>
2019-12-02 12:45:59 +0100

    migration

--- farmer/user_profile/urls.py
+++ farmer/user_profile/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.user_profile, name='user-profile'),
     path('add_product/', views.post_product, name='add-product'),
     path('search/', views.search_product, name='product-search'),
-    path('category', views.category_products, name='category')
+    path('category/', views.category_products, name='category'),
+    path('delete/<id>', views.delete_product, name='delete'),
 ]
 
