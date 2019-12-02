894b78c6bb54f512fa647cdd59163e6e09dc6b2a
arnorietdijk <arnorietdijk@users.noreply.github.com>
2019-12-02 09:18:31 +0100

    fixed inbox design

--- farmer/User/forms.py
+++ farmer/User/forms.py
@@ -26,10 +26,8 @@ class SignUpForm2(UserCreationForm):
     #username = forms.CharField(max_length=40, help_text='Enter a username')
     email = forms.EmailField(max_length=70, help_text='Enter a valid email adress')
     name = forms.CharField(max_length=60, required=False, help_text='Enter your full name')
-    address = forms.CharField(max_length=10, help_text='enter a valid address')
+    address = forms.CharField(max_length=20, help_text='enter a valid address')
    
     class Meta:
         model = User
         fields = ('username', 'name', 'address', 'email','password1', 'password2')
-
-
